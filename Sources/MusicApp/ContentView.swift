f2474c289863a3bcd8d4f5478b380b048fca4359
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-06-23 22:58:42 +0900

    set bottom tabViewBottomAccessory

--- Sources/MusicApp/ContentView.swift
+++ Sources/MusicApp/ContentView.swift
@@ -32,6 +32,7 @@ struct ContentView: View {
   @State var isPresentedNowPlayingView = false
   @State var selectedTabItem: TabItem = .home
   @State var nowPlayingState = NowPlayingState(music: .union)
+  @Namespace var namespace
   
   @ViewBuilder
   func tabView(_ tabItem: TabItem) -> some View {
@@ -44,6 +45,15 @@ struct ContentView: View {
     }
   }
   
+  var swipeUpGesture: some Gesture {
+    DragGesture()
+      .onEnded {
+        if $0.location.y < $0.startLocation.y {
+          isPresentedNowPlayingView.toggle()
+        }
+      }
+  }
+  
   var body: some View {
     TabView(selection: $selectedTabItem) {
       ForEach(TabItem.allCases, id: \.self) { tabItem in
@@ -54,64 +64,91 @@ struct ContentView: View {
         }
       }
     }
+    .tabBarMinimizeBehavior(.onScrollDown)
+    .tabViewBottomAccessory {
+      if let music = nowPlayingState.music {
+        NowPlayingBottomAccesory(music: music)
+          .navigationTransition(.zoom(sourceID: 1, in: namespace))
+          .tint(.primary)
+          .contentShape(.capsule)
+          .onTapGesture {
+            isPresentedNowPlayingView.toggle()
+          }
+          .padding(.leading, 10)
+          .padding(.trailing, 15)
+          .gesture(swipeUpGesture)
+      }
+    }
     .tint(.pink)
     .searchable(text: $text)
     .sheet(isPresented: $isPresentedNowPlayingView) {
       NowPlayingView()
-    }
-    .tabViewBottomAccessory {
-      if let music = nowPlayingState.music {
-        HStack {
-          AsyncImage(url: music.artworkURL) { image in
-            image
-              .resizable()
-              .frame(maxWidth: 30, maxHeight: 30)
-              .clipShape(.rect(cornerRadius: 10))
-
-          } placeholder: {
-            ProgressView()
-          }
-          .padding(.leading, 5)
-            
-          VStack(alignment: .leading, spacing: 0) {
-            Text(music.name)
-              .font(.caption)
-            Text(music.artist.name)
-              .foregroundStyle(.secondary)
-              .font(.caption2)
-          }
-          
-          Spacer()
-          
-          Button {
-            
-          } label: {
-            Image(systemName: "play.fill")
-          }
-          .tint(.primary)
-          
-          Spacer()
-            .frame(maxWidth: 15)
-          
-          Button {
-            
-          } label: {
-            HStack(spacing: 0) {
-              Image(systemName: "play.fill")
-              Image(systemName: "play.fill")
-            }
-          }
-          .tint(.primary)
-        }
-        .contentShape(.capsule)
-        .onTapGesture {
-          isPresentedNowPlayingView.toggle()
-        }
-        .padding(.leading, 10)
-        .padding(.trailing, 15)
-      }
+        .navigationTransition(.zoom(sourceID: 1, in: namespace))
     }
     .environment(nowPlayingState)
+  }
+}
+
+struct NowPlayingBottomAccesory: View {
+  let music: Music
+  
+  @Environment(\.tabViewBottomAccessoryPlacement) var tabViewBottomAccessoryPlacement
+  
+  var body: some View {
+    HStack(alignment: .center, spacing: 10) {
+      AsyncImage(url: music.artworkURL) { image in
+        image
+          .resizable()
+          .frame(maxWidth: 30, maxHeight: 30)
+          .clipShape(.rect(cornerRadius: 10))
+        
+      } placeholder: {
+        ProgressView()
+      }
+      .padding(.leading, 5)
+      
+      VStack(alignment: .leading, spacing: 0) {
+        Text(music.name)
+          .font(.caption)
+        Text(music.artist.name)
+          .foregroundStyle(.secondary)
+          .font(.caption2)
+      }
+      
+      Spacer()
+      
+      Button {
+        
+      } label: {
+        Image(systemName: "play.fill")
+      }
+      
+      switch self.tabViewBottomAccessoryPlacement {
+      case .inline:
+        Spacer()
+          .frame(maxWidth: 15)
+        Button {
+          
+        } label: {
+          HStack(spacing: 0) {
+            Image(systemName: "play.fill")
+            Image(systemName: "play.fill")
+          }
+        }
+      case .expanded: EmptyView()
+      default:
+        Spacer()
+          .frame(maxWidth: 15)
+        Button {
+          
+        } label: {
+          HStack(spacing: 0) {
+            Image(systemName: "play.fill")
+            Image(systemName: "play.fill")
+          }
+        }
+      }
+    }
   }
 }
 
